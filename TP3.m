1312dae35e8c96f055d2e1d2971c1a3c1caf77b6
Pierre Gabin FODOP GUMETE <pierre.fodop@ensta-bretagne.org>
2019-06-06 08:35:19 +0200

    probleme en spectre resolut

--- TP3.m
+++ TP3.m
@@ -1,12 +1,12 @@
-%% TP3 - filtrage anti-repliement par la méthode de la transposition bilinéaire
+%% TP3 - filtrage anti-repliement par la mÃ©thode de la transposition bilinÃ©aire
 
 close all
 clear all
 clc
 
 
-%% I Sous-échantillonnage et aliasing
-% 1.a Chargement et représentation temporelle
+%% I Sous-Ã©chantillonnage et aliasing
+% 1.a Chargement et reprÃ©sentation temporelle
 % A COMPLETER
 %chargement du son qui sera exploite
 [signal, fe] = audioread('glockenspiel.wav');
@@ -26,7 +26,7 @@ soundsc(signal, fe);
 % 1.b Calcul de la TFD et spectre en amplitude
 % A COMPLETER
 %calcul de la fft
-Nfft = 2^13;% on definit un grand nombre de valeur
+Nfft = 3*N;% on definit un grand nombre de valeur
 Fft_signal = fftshift(fft(signal, Nfft));
 %trace de son module
 figure;
@@ -36,7 +36,7 @@ xlabel('frequence en(Hz)');
 ylabel('amplitude');
 title('Module de la fft du signal de depart');
 
-% 1.c Sous-échantillonnage du signal
+% 1.c Sous-Ã©chantillonnage du signal
 % A COMPLETER
 %le sous echantillonage
 r = 4; % facteur de sous echantillonage
@@ -51,7 +51,7 @@ end
 % longue
 fer = fe/r; %definition de la frequence du signal sous echantillonee(au sous echantillonage)
 
-% 1.d Calcul de la TFD et spectre en amplitude du signal sous-échantillonné
+% 1.d Calcul de la TFD et spectre en amplitude du signal sous-Ã©chantillonnÃ©
 % A COMPLETER
 %calcul de la fft
 Fft_signal_sub = fftshift(fft(signal_sub, Nfft));
@@ -70,7 +70,7 @@ soundsc(signal_sub, fe);
 %signal en temps.
 
 %% II Gabarit
-% COMPLETER les paramètres du gabarit ci-dessous
+% COMPLETER les paramÃ¨tres du gabarit ci-dessous
 fc = fer/2;
 fp = fer/4;
 Gmin = 0.95;
@@ -86,16 +86,16 @@ xlim([0 fe/2])
 
 
 %% III Filtre de Butterworth
-% 3.a Calcul des paramètres du filtre
+% 3.a Calcul des paramÃ¨tres du filtre
 % - l'ordre N_b
-% - la fréquence de coupure (à -3dB) f0_b
-% COMPLETER les paramètres du filtre
+% - la frÃ©quence de coupure (Ã  -3dB) f0_b
+% COMPLETER les paramÃ¨tres du filtre
 N_b  = (log10(10^(Gmax/10)-1) - log10(10^(Gmin/10)-1))/(2*log10(0.5)); 
 N_b = ceil(N_b);%passage a la partie entiere
 f0_b = fp/( (10^(Gmax/10)-1)^(1/(2*N_b)));
 
 %-------------------------------------PARTIE DONNEE : PAS TOUCHE !
-% Création et visualisation de la fonction de transfert
+% CrÃ©ation et visualisation de la fonction de transfert
 [b_b,a_b] = butter(N_b,f0_b/(fe/2)); 
 [H_butter,f_butter] = freqz(b_b,a_b,'whole',N);
 figure;
@@ -125,7 +125,7 @@ rep_imp_filtre = ifft(H_butter);
 signal_filtre = zeros(N*2-1,2);
 signal_filtre(:,1) = conv(rep_imp_filtre,signal(:,1));
 signal_filtre(:,2) = conv(rep_imp_filtre,signal(:,2));
-% 3.c Sous-échantillonnage
+% 3.c Sous-Ã©chantillonnage
 % A COMPLETER
 N2 = length(signal_filtre);
 r2 = 4; % facteur de sous echantillonage
@@ -150,17 +150,17 @@ title('module de la fft du signal filtre Butterworth sous echantillone');
 soundsc(signal_filtre, fer2);
 
 %% IV Filtre de Tchebychev
-% 4.a et 4.b Calcul des parmatères du filtre
-% - la fréquence de coupure f0_t
+% 4.a et 4.b Calcul des parmatÃ¨res du filtre
+% - la frÃ©quence de coupure f0_t
 % - le taux d'ondulation epsilon2
 % - l'ordre du filtre N_t
-% COMPLETER les paramètres du filtre
+% COMPLETER les paramÃ¨tres du filtre
 f0_t     = fp;
 epsilon2 = 10^(Gmax/10)-1;
 N_t      = acos((10^(Gmin/10)-1)*epsilon2^2*f0_t/fc)/acos(f0_t/fc);
 N_t = ceil(N_t);
 %-------------------------------------PARTIE DONNEE : PAS TOUCHE !
-% Création du filtre et visualistion de la fonction de transfert
+% CrÃ©ation du filtre et visualistion de la fonction de transfert
 [b_t,a_t] = cheby1(N_t,10*log10(1/Gmin),f0_t/(fe/2));
 [H_cheby,f_cheby] = freqz(b_t,a_t,'whole',N);
 figure;
@@ -191,7 +191,7 @@ signal_filtre2 = zeros(N*2-1,2);
 signal_filtre2(:,1) = conv(rep_imp_filtre2,signal(:,1));
 signal_filtre2(:,2) = conv(rep_imp_filtre2,signal(:,2));
 
-% 4.d Sous-échantillonnage
+% 4.d Sous-Ã©chantillonnage
 % A COMPLETER
 N2 = length(signal_filtre2);
 r2 = 4; % facteur de sous echantillonage
